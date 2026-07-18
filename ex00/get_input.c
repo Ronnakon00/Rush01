5bea859d82bc1dc78f85b2feff726af98df678a7
Ronny <kamolkluaykaeo@gmail.com>
2026-07-18 14:23:30 +0700

    fix_getinput

--- ex00/get_input.c
+++ ex00/get_input.c
@@ -1,19 +1,19 @@
 int	read_data_a_organize_data(int *result, char *str)
 {
 	int	i;
-	int j;
+	int	j;
 	int	count;
+
 	i = 0;
 	j = 0;
 	count = 0;
-
 	while (str[i] != '\0')
 	{
 		if (str[i] >= '1' && str[i] <= '4')
 		{
-			if (str[i + 1] != '\0' && count == 16)
+			if (count >= 16)
 				break ;
-			if (str[i + 1] != ' ' && count < 16)
+			if (str[i + 1] != ' ')
 				break ;
 			result[j] = str[i] - '0';
 			j++;
